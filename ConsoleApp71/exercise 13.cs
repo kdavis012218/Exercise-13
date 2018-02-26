830879cb88524d0011fd856333b7fcb3a0cf7648
kdavis012218 <35358468+kdavis012218@users.noreply.github.com>
2018-02-25 20:07:20 -0500

    exercise 13

---
+++ ConsoleApp71/exercise 13.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp71
+{
+    class Program
+    {
+        static void Main(string[] args)
+          
+        {
+            Console.WriteLine("Please enter a number");
+            Console.ReadLine();
+            int x;
+            for (int a = 1; x = x - 1; x--)
+            {
+                Console.WriteLine();
+                {
+
+                    Console.ReadKey();
+
+                }
+            }
+        }
